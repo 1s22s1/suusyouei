d171e9608243b20b385ca3b76a04a3baad4c1e8f
1s22s1 <1s22s1@@users.noreply.github.com>
2023-01-01 21:58:19 +0900

    Add Question 4

--- test_answer.jl
+++ test_answer.jl
@@ -1,25 +1,60 @@
 module TestAnswer
+using Test
 
-    using Test
-    
-    include("answer.jl")
+include("answer.jl")
 
-    function main()
+function main()
+    @testset "Divisor" begin
         @test Answer.divisor(6) == [1, 2, 3, 6]
         @test Answer.divisor(7) == [1, 7]
-        @test Answer.q1(583305) == [1, 3, 5, 15, 37, 111, 185, 555, 1051, 3153, 5255, 15765, 38887, 116661, 194435, 583305] 
+    end
 
-        @test Answer.isabundantnumber(6) == false
+    @testset "Sum of divisior" begin
         @test Answer.isabundantnumber(12) == true
-        @test Answer.q2() == 100000
+        @test Answer.isabundantnumber(6) == false
 
+        @test Answer.isdeficientnumber(8) == true
+        @test Answer.isdeficientnumber(12) == false
+    end
+
+    @testset "Prime" begin
+        @test Answer.isprime(3) == true
+        @test Answer.isprime(4) == false
+    end
+
+    @testset "Collatz" begin
         @test Answer.collatz(1) == [1, 4, 2, 1]
         @test Answer.collatz(2) == [2, 1]
-        @test Answer.q3() == 20830
     end
+
+    @testset "Kaitou" begin
+        @test Answer.q1(583305) == [
+            1,
+            3,
+            5,
+            15,
+            37,
+            111,
+            185,
+            555,
+            1051,
+            3153,
+            5255,
+            15765,
+            38887,
+            116661,
+            194435,
+            583305,
+        ]
+        @test Answer.q2() == 100000
+        @test Answer.q3() == 20830
+        @test Answer.q4(1356361) == false
+    end
+end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
     using .TestAnswer
+
     TestAnswer.main()
 end
