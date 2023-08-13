6d190787673824b2000d30ada230da7a527180d3
1s22s1 <1s22s1@example.com>
2023-08-13 22:58:40 +0900

    Rewrite q1

--- answer_test.go
+++ answer_test.go
@@ -15,8 +15,28 @@ func Test_addNumbers(t *testing.T) {
 	}
 }
 
-func Test_q1(t *testing.T) {
-	if !reflect.DeepEqual(q1(583305), []int{1, 3, 5, 15, 37, 111, 185, 555, 1051, 3153, 5255, 15765, 38887, 116661, 194435, 583305}) {
-		t.Error("実際の結果が異なります。得られた結果＝＞", q1(583305))
+func TestQ1(t *testing.T) {
+	type args struct {
+		n int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "normal",
+			args: args{n: 583305},
+			want: []int{1, 3, 5, 15, 37, 111, 185, 555, 1051, 3153, 5255, 15765, 38887, 116661, 194435, 583305},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := q1(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("q1() + %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
