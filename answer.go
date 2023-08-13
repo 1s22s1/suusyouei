8e08fd53a8bfd60119fee971e55b77433d583b9f
1s22s1 <1s22s1@example.com>
2023-08-13 10:19:30 +0900

    Resolve Q1

--- answer.go
+++ answer.go
@@ -1,5 +1,17 @@
 package answer
 
-func addNumbers(x, y int) int {
-	return x + x
+func divisor(n int) []int {
+	var x []int
+
+	for i := 1; i <= n; i++ {
+		if (n % i) == 0 {
+			x = append(x, i)
+		}
+	}
+
+	return x
+}
+
+func q1(n int) []int {
+	return divisor(n)
 }
--- answer_test.go
+++ answer_test.go
@@ -1,11 +1,22 @@
 package answer
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_addNumbers(t *testing.T) {
-	result := addNumbers(2, 3)
+	if !reflect.DeepEqual(divisor(6), []int{1, 2, 3, 6}) {
+		t.Error("実際の結果が異なります。得られた結果＝＞", divisor(6))
+	}
 
-	if result != 5 {
-		t.Error("実際の結果が異なります。得られた結果＝＞", result)
+	if !reflect.DeepEqual(divisor(7), []int{1, 7}) {
+		t.Error("実際の結果が異なります。得られた結果＝＞", divisor(7))
+	}
+}
+
+func Test_q1(t *testing.T) {
+	if !reflect.DeepEqual(q1(583305), []int{1, 3, 5, 15, 37, 111, 185, 555, 1051, 3153, 5255, 15765, 38887, 116661, 194435, 583305}) {
+		t.Error("実際の結果が異なります。得られた結果＝＞", q1(583305))
 	}
 }
